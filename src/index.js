e9966a0e85f042c12759d52d89b16ab6960384fa
Ane Acevedo <ane.acevedo@gmail.com>
2021-03-25 13:27:16 -0300

    Consumo de API por medio de la barra de búsqueda, errores y mensajes de error, comenzando a mostrar la información encontrada en pantalla (ya se muestra por consola)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
-//Llamar a la api
-const url = 'https://restcountries.eu/rest/v2/all';
+const shows = document.getElementById('show');
+const template = document.getElementById('results').content;
+const fragment = document.createDocumentFragment();
 
-//Consumo de la api
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => console.log(err));
+//Escuchar evento de la barra de búsuqueda (asíncrona)
+document.addEventListener('keypress', async e => {
+    if (e.target.matches('#searchBar')) {
+        // console.log(e.key)
+        if (e.key === 'Enter') {
+            // console.log('escucho')
+
+            //Consumo de la API
+            try {
+                shows.innerHTML = `<h3>Cargando...</h3>`;
+
+                let query = e.target.value.toLowerCase(),
+                    url = `https://restcountries.eu/rest/v2/name/${query}`,
+                    response = await fetch(url),
+                    json = await response.json();
+                //console.log(url, response, json);
+
+                //Enviar error con throw para ser capturado en el catch (código y mensaje)
+                if (!response.ok) throw {
+                    status: response.status,
+                    statusText: response.statusText
+                }
+
+                if (json.lenght === 0) {
+                    shows.innerHTML = `<p>No hay resultados que coincidan con tu búsqueda, inténtalo de nuevo.</p>`
+                } else {
+                    json.forEach(element => {
+                        template.querySelector('h3').textContent = element.name;
+                    });
+                }
+
+
+
+            } catch (err) {
+                console.log(err);
+                const message = err.statusText || "Hubo un error, intenta con una nueva búsqueda.";
+                shows.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
+                if (err.status === 404) {
+                    shows.innerHTML = `<p>No hay resultados que coincidan con tu búsqueda, inténtalo de nuevo.</p>`
+                }
+
+
+            }
+        }
+    }
+})
+
+//URL api
+// const url = 'https://restcountries.eu/rest/v2/name/chile?fullText=true';
+
+
+// // Consumo de la api
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(err => console.log(err));
